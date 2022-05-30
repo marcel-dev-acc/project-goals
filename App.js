b487f64f190a97d8cc3d6395511f11c38d2572d0
= <=>
2022-05-30 21:31:03 +0100

    Create the goals app and validated functionality

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { View } from 'react-native';
 import Title from './components/title/Title';
 import Goal from './components/goals/Goals';
+import { testVar } from './env';
 
 const App = () => {
   // Used to define the routes in the project
--- components/goals/Goals.js
+++ components/goals/Goals.js
@@ -1,19 +1,101 @@
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import DisplayGoals from './display/Display';
-import CreateGoal from './create/Create';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  Button
+} from 'react-native';
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const Goals = () => {
   const [goals, setGoals] = useState([]);
+  const [text, setText] = useState('');
+
+  const renderItem = ({ item }) => (
+    <Item title={item.text} />
+  );
+
   return (
     <View>
-      <CreateGoal />
-      <DisplayGoals />
+      <Text style={styles.text}>Set a goal</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={
+          (eventText) => {
+            setText(eventText);
+          }
+        }
+        value={text}
+      />
+      <View style={styles.buttonView}>
+        <Button
+          title="Press Me"
+          color='#B601B8'
+          onPress={
+            () => {
+              goals.unshift({
+                timestamp: new Date(),
+                text: text
+              });
+              setGoals(goals);
+              setText('');
+            }
+          }
+        />
+      </View>
+      <FlatList
+        style={styles.flatList}
+        data={goals}
+        renderItem={renderItem}
+        keyExtractor={item => item.timestamp}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatList: {
+    marginTop: 20,
+    height: 350,
+  },
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 20,
+    color: '#000000'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#B601B8',
+    padding: 10,
+  },
+  buttonView: {
+    maxWidth: 150,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+});
 
 export default Goals;
